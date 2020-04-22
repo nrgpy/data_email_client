9edad610f921f451f5be6aeebd52a44c25cd626b
Dave <davec.vt@gmail.com>
2020-04-22 15:57:57 -0400

    init repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# ![NRGPy](https://www.gravatar.com/avatar/6282094b092c756acc9f7552b164edfe?s=24) email_client 
+
+**email_client** is a Python script for downloading data files from emails.
+
+
+## Examples
+
+see script docstrings for usage.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+setup(
+      name='email_client',
+      version='0.1.',
+      description='email imap client for downloading email data files',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/nrgpy/email_client',
+      author='NRG Systems, Inc.',
+      author_email='support@nrgsystems.com',
+      keywords='nrg systems rld symphonie symphoniepro wind data spidar remote sensor lidar email imap',
+      packages=[
+            'nrgpy'
+      ],
+      install_requires={
+      ],
+      python_requires>='3.5',
+      zip_safe=False,
+      classifiers=[
+          'License :: OSI Approved :: MIT License'
+      ]
+)
